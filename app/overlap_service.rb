9bdd897868422689a5a2286dbc81a48d95a5355b
david <david.jaress@gmail.com>
2016-03-08 23:51:33 -0600

    Create service to calculate Morisita Overlap for each blog pair

--- app/overlap_service.rb
+++ app/overlap_service.rb
@@ -1,6 +1,93 @@
+require 'mathn'
 require_relative 'application'
 
 class OverlapService
-  def calculate_overlap
+  def calculate_all_overlaps
+    blog_pairs = Blog.all.pluck(:id).to_a.combination(2).to_a
+    blog_pairs.each do |blog_id_array|
+      first_blog_id = blog_id_array[0]
+      second_blog_id = blog_id_array[1]
+      next if Comparison.find_by(first_blog_id: first_blog_id, second_blog_id: second_blog_id)
+      overlap = calculate_overlap(Blog.find(first_blog_id), Blog.find(second_blog_id))
+
+      Comparison.create(
+        first_blog_id: first_blog_id,
+        second_blog_id: second_blog_id,
+        overlap: overlap
+      )
+    end
+  end
+
+  def calculate_overlap(first_blog_id, second_blog_id)
+    first_blog = Blog.find(first_blog_id)
+    second_blog = Blog.find(second_blog_id)
+    numerator(first_blog, second_blog) / denominator(first_blog, second_blog)
+  end
+
+  def numerator(first_blog, second_blog)
+    2 * series(first_blog, second_blog)
+  end
+
+  def denominator(first_blog, second_blog)
+    simpson_sum = (simpson_index_for(first_blog) + simpson_index_for(second_blog))
+    total_post_product = reblogged_post_count_for(first_blog) * reblogged_post_count(second_blog)
+    simpson_sum * total_post_product
+  end
+
+  def simpson_sum(blog)
+    # D_x + D_y
+    simpson_index_for(blog)
+  end
+
+  def series(first_blog, second_blog)
+    sum = 0
+    uniq_sources = unique_sources_for(first_blog)
+    uniq_sources.each { |source| sum += source_count_product(first_blog, second_blog, source) }
+    sum
+  end
+
+  def source_count_product(first_blog, second_blog, source)
+    # x_i * y_i
+    source_count_for(first_blog, source) * source_count_for(second_blog, source)
+  end
+
+  def simpson_index_for(blog)
+    # D_x
+    sum = 0
+    uniq_sources = unique_sources_for(blog)
+    uniq_sources.each { |source| sum += simpson_sub_index_for(blog, source) }
+    sum
+  end
+
+  def simpson_sub_index_for(blog, source_title)
+    # x_i(x_i -1) / X(X-1)
+    source_count = source_count_for(blog, source_title)
+    reblogged_post_count = reblogged_post_count_for(blog)
+    numerator = source_count * (source_count - 1)
+    denominator = reblogged_post_count * (reblogged_post_count - 1)
+    numerator / denominator
+  end
+
+  def reblogged_post_count_for(blog)
+    # X
+    reblogged_posts_for(blog).count
+  end
+
+  def reblogged_posts_for(blog)
+    blog.posts.where.not(source_title: nil).where.not(source_title: '')
+  end
+
+  def unique_source_count_for(blog)
+    # S
+    unique_sources_for(blog).count
+  end
+
+  def unique_sources_for(blog)
+    reblogged_posts_for(blog).pluck(:source_title).uniq
+  end
+
+  def source_count_for(blog, source_title)
+    # x_i, y_i
+    blog.posts.where(source_title: source_title).count
   end
 end
--- spec/app/overlap_service_spec.rb
+++ spec/app/overlap_service_spec.rb
@@ -1,6 +1,93 @@
 require_relative '../spec_helper'
 
 RSpec.describe OverlapService do
+  let(:subject) { OverlapService.new }
+  let(:first_blog) { Blog.create(url: 'spam') }
+  let(:second_blog) { Blog.create(url: 'baz') }
+
   describe :calculate_overlap do
+  end
+
+  describe :unique_source_count_for do
+    it 'returns count of unique source titles' do
+      Post.create(blog_id: first_blog.id, source_title: '')
+      Post.create(blog_id: first_blog.id, source_title: nil)
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+
+      expect(subject.unique_source_count_for(first_blog)).to eq 1
+    end
+  end
+
+  describe :source_count_for do
+    it 'returns count of times a source appears in a blog' do
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+
+      expect(subject.source_count_for(first_blog, 'bar')).to be 2
+    end
+  end
+
+  describe :reblogged_post_count_for do
+    it 'returns a count of posts with a source title' do
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+      Post.create(blog_id: first_blog.id, source_title: '')
+      Post.create(blog_id: first_blog.id, source_title: nil)
+
+      expect(subject.reblogged_post_count_for(first_blog)).to be 2
+    end
+  end
+
+  describe :simpson_index_for do
+    it 'sums the sub indices' do
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+      Post.create(blog_id: first_blog.id, source_title: nil)
+
+      result = subject.simpson_index_for(first_blog)
+      expect(result).to eq 2 / 5
+    end
+  end
+
+  describe :simpson_sub_index_for do
+    it 'calculates the index correctly' do
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+      Post.create(blog_id: first_blog.id, source_title: nil)
+
+      result = subject.simpson_sub_index_for(first_blog, 'bar')
+      expect(result).to eq 1 / 3
+    end
+  end
+
+  describe :source_count_product do
+    it 'returns product of the source counts' do
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: second_blog.id, source_title: 'bar')
+      Post.create(blog_id: second_blog.id, source_title: 'foo')
+
+      result = subject.source_count_product(first_blog, second_blog, 'bar')
+      expect(result).to eq 2
+    end
+  end
+
+  describe :series do
+    it 'returns the sum of the source count products for each unique post' do
+      Post.create(blog_id: first_blog.id, source_title: 'bar')
+      Post.create(blog_id: first_blog.id, source_title: 'foo')
+      Post.create(blog_id: second_blog.id, source_title: 'bar')
+      Post.create(blog_id: second_blog.id, source_title: 'bar')
+      Post.create(blog_id: second_blog.id, source_title: 'foo')
+
+      result = subject.series(first_blog, second_blog)
+      expect(result).to eq 3
+    end
   end
 end
