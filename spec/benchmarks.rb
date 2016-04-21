d77658cfe308f2a2087e629f3389330a46aa1ed0
david <david.jaress@gmail.com>
2016-04-21 14:53:21 -0500

    Add blogs for benchmarking. DRY up benchmark code

--- spec/benchmarks.rb
+++ spec/benchmarks.rb
@@ -4,58 +4,52 @@ require_relative '../app/application'
 class Benchmarker
   def initialize
     @newest_time = Time.now.to_i
-    @oldest_time = (Time.now - 7000000).to_i
-    @first_blog = Blog.create!(url: 'congressarchives.tumblr.com')
-    @second_blog = Blog.create!(url: 'whitehouse.tumblr.com')
-    @third_blog = Blog.create!(url: 'statedept.tumblr.com')
+    @oldest_time = (Time.now - 5000000).to_i
+    @blog_array = [
+      Blog.create!(url: 'congressarchives.tumblr.com'),
+      Blog.create!(url: 'whitehouse.tumblr.com'),
+      Blog.create!(url: 'statedept.tumblr.com'),
+      Blog.create!(url: 'nasa.tumblr.com'),
+      Blog.create!(url: 'spaceforeurope.tumblr.com')
+    ]
   end
 
-  def poll_blogs(blog_array)
-    blog_array.each { |b| BlogPoller.new(b, @newest_time, @oldest_time).poll_posts }
+  def poll_blogs
+    @blog_array.each { |b| BlogPoller.new(b, @newest_time, @oldest_time).poll_posts }
   end
 
   def make_tumblr_api_request(blog)
-    poller = BlogPoller.new(@first_blog, @newest_time, @oldest_time)
+    poller = BlogPoller.new(@blog_array.first, @newest_time, @oldest_time)
     @posts = poller.post_list
   end
 
   def save_posts_to_db(posts)
-    poller = BlogPoller.new(@first_blog, @newest_time, @oldest_time)
+    poller = BlogPoller.new(@blog_array.first, @newest_time, @oldest_time)
     poller.parse_post_list(posts)
   end
 
-  def calculate_simpson_indices(blogs)
-    blogs.each { |b| SimpsonService.new.calculate_and_save_index(b) }
+  def calculate_simpson_indices
+    @blog_array.each { |b| SimpsonService.new.calculate_and_save_index(b) }
   end
 
   def run_benchmarks
-    blogs = [@first_blog, @second_blog, @third_blog]
-
-    @first_blog.posts.destroy_all
-    @second_blog.posts.destroy_all
-    @third_blog.posts.destroy_all
+    @blog_array.each { |b| b.posts.destroy_all }
 
     Benchmark.bm do |bm|
       puts 'API call benchmark'
-      bm.report { make_tumblr_api_request(@first_blog) }
+      bm.report { make_tumblr_api_request(@blog_array.first) }
 
       puts 'Post parse and save benchmark'
       bm.report { save_posts_to_db(@posts) }
 
-      @first_blog.posts.destroy_all
-      @first_blog.update!(completed: false)
-
-      @second_blog.posts.destroy_all
-      @second_blog.update!(completed: false)
-
-      @third_blog.posts.destroy_all
-      @third_blog.update!(completed: false)
+      @blog_array.each  { |b| b.posts.destroy_all }
+      @blog_array.each  { |b| b.update!(completed: false) }
 
       puts 'Integration benchmark'
-      bm.report { poll_blogs(blogs) }
+      bm.report { poll_blogs }
 
       puts 'Simpson benchmark'
-      bm.report { calculate_simpson_indices(blogs) }
+      bm.report { calculate_simpson_indices }
 
       puts 'Morishita benchmark'
       bm.report { OverlapService.new.calculate_all_overlaps }
